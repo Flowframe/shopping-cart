94b3f776b0594b2976748c1f8479d5dd83a9c015
Lars Klopstra <larsklopstra@gmail.com>
2021-04-12 16:26:15 +0200

    wip

--- tests/Feature/ShoppingCartTest.php
+++ tests/Feature/ShoppingCartTest.php
@@ -2,6 +2,7 @@
 
 namespace Flowframe\ShoppingCart\Tests\Features;
 
+use Flowframe\ShoppingCart\ShoppingCart;
 use Flowframe\ShoppingCart\Tests\TestCase;
 
 class ShoppingCartTest extends TestCase
@@ -9,61 +10,61 @@ class ShoppingCartTest extends TestCase
     /** @test */
     public function it_can_perform_a_simple_checkout_flow(): void
     {
-        cart()->addItem([
+        ShoppingCart::items()->add([
             'id' => 1,
             'name' => 'Shoes',
             'price' => 100,
             'vat' => 10,
         ]);
 
-        cart()->addItem([
+        ShoppingCart::items()->add([
             'id' => 2,
             'name' => 'Jeans',
             'price' => 50,
             'vat' => 10,
         ]);
 
-        $this->assertEquals(165, cart()->total(withVat: true));
+        $this->assertEquals(165, ShoppingCart::total(withVat: true));
 
-        $this->assertEquals(150, cart()->total(withVat: false));
+        $this->assertEquals(150, ShoppingCart::total(withVat: false));
 
-        cart()->addCoupon([
+        ShoppingCart::coupons()->add([
             'id' => '50%-off',
             'name' => '50% off',
             'value' => 50,
             'type' => 'percentage',
         ]);
 
-        cart()->addCoupon([
+        ShoppingCart::coupons()->add([
             'id' => 'first-time-discount',
             'name' => '$10 off',
             'value' => 10,
             'type' => 'fixed',
         ]);
 
-        cart()->addFee([
+        ShoppingCart::fees()->add([
             'id' => 'shipping',
             'name' => 'Global shipping',
             'price' => 50,
             'vat' => 10,
         ]);
 
-        $this->assertEquals(2, cart()->items()->count());
+        $this->assertEquals(2, ShoppingCart::items()->count());
 
         // without vat ((100 * 0.5 - 10)) + ((50 * 0.5 - 10)) + (50) = 105
-        $this->assertEquals(105, cart()->total(withVat: false));
+        $this->assertEquals(105, ShoppingCart::total(withVat: false));
 
         // with vat ((100 * 0.5 - 10) * 1.1) + ((50 * 0.5 - 10) * 1.1) + (50 * 1.1) = 115.5
-        $this->assertEquals(115.5, cart()->total(withVat: true));
+        $this->assertEquals(115.5, ShoppingCart::total(withVat: true));
 
-        cart()->removeCoupon('50%-off');
+        ShoppingCart::coupons()->remove('50%-off');
 
-        cart()->removeCoupon('first-time-discount');
+        ShoppingCart::coupons()->remove('first-time-discount');
 
         // without vat 100 + 50 + 50 = 200
-        $this->assertEquals(200, cart()->total(withVat: false));
+        $this->assertEquals(200, ShoppingCart::total(withVat: false));
 
         // with vat (100 * 1.1) + (50 * 1.1) + (50 * 1.1) = 220
-        $this->assertEquals(220, cart()->total(withVat: true));
+        $this->assertEquals(220, ShoppingCart::total(withVat: true));
     }
 }
