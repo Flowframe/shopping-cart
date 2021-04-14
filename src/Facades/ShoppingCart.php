dd0e5b62e4b77ebc108478688e59f40696b7f410
Shuvro Roy <shuvro.nsu.cse@gmail.com>
2021-04-14 22:46:23 +0600

    Added missing default values in docblock

--- src/Facades/ShoppingCart.php
+++ src/Facades/ShoppingCart.php
@@ -8,9 +8,9 @@ use Illuminate\Support\Facades\Facade;
  * @method static \Flowframe\ShoppingCart\Managers\ItemManager items()
  * @method static \Flowframe\ShoppingCart\Managers\FeeManager fees()
  * @method static \Flowframe\ShoppingCart\Managers\CouponManager coupons()
- * @method static float subtotal()
+ * @method static float subtotal(bool $withVat = true)
  * @method static void empty()
- * @method static float total()
+ * @method static float total(bool $withVat = true, bool $withFees = true, bool $withCoupons = true)
  * 
  * @see \Flowframe\ShoppingCart\ShoppingCart
  */
